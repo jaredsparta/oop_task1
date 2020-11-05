7079b8e50211dfc789c8b52c07eb72c027f5414b
jared <jtlss14@gmail.com>
2020-11-05 14:27:54 +0000

    was informed we only neded one parent class

--- task.py
+++ task.py
@@ -1,32 +1,17 @@
-class Fizz:
-    def __init__(self):
-        pass
-
-    # Returns a FIZZ if a multiple of 3
-    def fizz(self, number):
-        if number % 3 == 0:
-            return "FIZZ"
-
-
-class Buzz:
-    def __init__(self):
-        pass
-
-        # Returns BUZZ if number is multiple of 5
-    def buzz(self, number):
-        if number % 5 == 0:
-            return "BUZZ"
-
 class FizzBuzz:
-    def __init__(self):
+    def __init__(self, final_number):
         pass
+    
+    def play(self, final_number):
+        for number in range(1, final_number + 1):
+            if number % 3 == 0 and number % 5 == 0:
+                print("FIZZBUZZ")
+            elif number % 3 == 0:
+                print("FIZZ")
+            elif number % 5 == 0:
+                print ("BUZZ")
+            else:
+                print(number)
 
-    # Returns FIZZBUZZ if number is multiple of both 3 and 5
-    def buzz(self, number):
-        if number % 5 == 0 and number % 3 == 0:
-            return "FIZZBUZZ"
 
-class Game(Fizz, Buzz, FizzBuzz):
-    def __init__(self, max_number):
-        pass
-
+FizzBuzz(100)
